c004243ad38cb8a1b8f4403b5cf8dc50d0ebe927
Richard Asiamah <123144205+Rich-Machine@users.noreply.github.com>
2024-11-21 15:14:47 -0500

    Update demonstration.jl

--- demonstration.jl
+++ demonstration.jl
@@ -2,23 +2,32 @@ using PowerModels
 using Ipopt
 using JuMP
 using PowerPlots
-using PowerModelsAnalytics
 
 # Load the data
 data = PowerModels.parse_file("data/ghana_test_case.m")
 # data = PowerModels.parse_file("data/hawaii.m")
-powerplot(data)
+powerplot(data, bus_size=50, branch_size =2, gen_size = 50, load_size = 50; width=300, height=500)
+
+
+for key in keys(data["gen"])
+    data["gen"][key]["pmin"] = 0
+end
+
+for key in keys(data["gen"])
+    data["gen"][key]["qmin"] = 10 * data["gen"][key]["qmin"]
+    data["gen"][key]["qmax"] = 10 * data["gen"][key]["qmax"]
+end
 
 # Create the model
-pm = instantiate_model(data, DCPPowerModel, PowerModels.build_pf)
+pm = instantiate_model(data, ACPPowerModel, PowerModels.build_opf)
 result = optimize_model!(pm, optimizer=Ipopt.Optimizer)
 
 # Print the results
 PowerModels.print_summary(result["solution"])
 
 # Ensure total generation is greater than total load
-total_gen = sum(data["gen"][g]["qmax"] for g in keys(data["gen"]))
-total_load = sum(data["load"][l]["qd"] for l in keys(data["load"]))
+total_gen = sum(data["gen"][g]["pg"] for g in keys(data["gen"]))
+total_load = sum(data["load"][l]["pd"] for l in keys(data["load"]))
 if total_gen >= total_load
     println("Total generation is greater than total load")
 else
@@ -27,5 +36,6 @@ end
 
 # Relax the branch flow limits
 for key in keys(data["branch"])
-    data["branch"][key]["rate_a"] = 1.5 * data["branch"][key]["rate_a"]
+    data["branch"][key]["rate_a"] = 15 * data["branch"][key]["rate_a"]
 end
+
