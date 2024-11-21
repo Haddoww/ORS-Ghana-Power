d0aae5d8b3160fe3b41851516fbd03d792595c4a
Richard Asiamah <123144205+Rich-Machine@users.noreply.github.com>
2024-11-20 23:37:48 -0500

    Update demonstration.jl

--- demonstration.jl
+++ demonstration.jl
@@ -16,3 +16,16 @@ result = optimize_model!(pm, optimizer=Ipopt.Optimizer)
 # Print the results
 PowerModels.print_summary(result["solution"])
 
+# Ensure total generation is greater than total load
+total_gen = sum(data["gen"][g]["qmax"] for g in keys(data["gen"]))
+total_load = sum(data["load"][l]["qd"] for l in keys(data["load"]))
+if total_gen >= total_load
+    println("Total generation is greater than total load")
+else
+    println("Total generation is less than total load")
+end
+
+# Relax the branch flow limits
+for key in keys(data["branch"])
+    data["branch"][key]["rate_a"] = 1.5 * data["branch"][key]["rate_a"]
+end
