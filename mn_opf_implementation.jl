c509968433b5f989811702d61f3119523c9dae10
Richard Asiamah <123144205+Rich-Machine@users.noreply.github.com>
2025-02-01 01:09:46 -0500

    Create mn_opf_implementation.jl
    This file executes the multiperiod/multinetwork optimal power flow using the functions defined in multiperiod_opf.jl. I have provided the skeleton of the code, but team members will need to fill out the sections of the code. Find the data to complete it and conduct analyses of the results obtained.

---
+++ mn_opf_implementation.jl
@@ -0,0 +1,37 @@
+using PowerModels
+using Ipopt
+using JuMP
+using Gurobi
+
+# Load the data
+data = PowerModels.parse_file("GhanaGrid.m")
+data = PowerModels.replicate(data, 24) ## this replicate function creates duplicate data for 24 hours
+
+## Vary the load demand for each of the 24 hours - Hadley 
+scale = 1 ## this is the scale factor for the load demand
+for nw in keys(data["nw"])
+    for key in keys(data["nw"][nw]["load"])
+        data["nw"][nw]["load"][key]["pd"] = data["nw"][nw]["load"][key]["pd"] * scale ## the scale number depends on the profile choose; currently set to 1
+        data["nw"][nw]["load"][key]["qd"] = data["nw"][nw]["load"][key]["qd"] * scale
+    end
+end
+
+## Vary the solar generation for each of the 24 hours - Tejaswi
+scale = 1 ## this is the scale factor for the solar generation
+for nw in keys(data["nw"])
+    for key in keys(data["nw"][nw]["gen"]) ## you have to change this to just the solar generation indices
+        data["nw"][nw]["gen"][key]["pg"] = data["nw"][nw]["gen"][key]["pg"] * scale ## the scale number depends on the profile choose
+        data["nw"][nw]["gen"][key]["qg"] = data["nw"][nw]["gen"][key]["qg"] * scale
+    end
+end
+
+## Solve OPF repeatedly for each of the 24 hours
+result = solve_mp_opf_ramp(data, ACPPowerModel, Ipopt.Optimizer, multinetwork=true)
+
+## Play with the results - Karen
+## Investigate changes in output for current VS projeccted solar installations
+println("Total cost")
+println("Total loss")
+println("Total solar generation")
+println("Total load demand")
+
